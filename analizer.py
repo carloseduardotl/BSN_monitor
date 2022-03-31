c573f6eebc8aa3cd489740b79ca93c89c9cbb144
Carlos Eduardo Taborda Lottermann <cadutaborda@gmail.com>
2022-03-31 16:40:47 -0300

    First implementation of analizer.py

---
+++ analizer.py
@@ -0,0 +1,30 @@
+
+raw_path = "data/raw/"
+
+engine = "reli_engine.txt"
+g3t1_1 = "g3t1_1.txt"
+g3t1_2 = "g3t1_2.txt"
+g3t1_3 = "g3t1_3.txt"
+g3t1_4 = "g3t1_4.txt"
+g3t1_5 = "g3t1_5.txt"
+g3t1_6 = "g3t1_6.txt"
+g4t1 = "g4t1.txt"
+param_adapter = "param_adapter.txt"
+enactor = "enactor.txt"
+injector = "injector.txt"
+logger = "logger.txt"
+patient = "patient.txt"
+collector = "collector.txt"
+data_access = "data_access.txt"
+rosout = "rosout.txt"
+master = "master.txt"
+
+modules = [engine, g3t1_1, g3t1_2, g3t1_3, g3t1_4, g3t1_5, g3t1_6, g4t1, param_adapter, enactor, injector, logger, patient, collector, data_access, rosout, master]
+
+#for module in modules:
+#	print(module)
+
+
+#with open(raw_path+collector, "r") as arquivo:
+#	text = arquivo.read()
+#print(text)
